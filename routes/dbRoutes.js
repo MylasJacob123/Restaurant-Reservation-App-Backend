8c24811b0a2ffe6752b47e77a114018fd27cd718
MylasJacob123 <mylasjacob18.5@gmail.com>
2025-01-08 12:23:05 +0200

    Role-Based Access Control (RBAC) and Admin Routes (Restaurant and Reservation Management) have been implemented and work

--- routes/dbRoutes.js
+++ routes/dbRoutes.js
@@ -9,18 +9,20 @@ const {
   updateReservation,
   deleteReservation
 } = require("../controllers/dbController");
+const { protect } = require("../middleware/authMiddleware");
+const { adminOnly } = require("../middleware/roleMiddleware");
 const router = express.Router();
 
 // RESTAURANT ROUTES
 router.get("/get-restaurants", getRestaurants);
-router.post("/add-restaurant", addRestaurant);
-router.put("/update-restaurant/:id", updateRestaurant);
-router.delete("/delete-restaurant/:id", deleteRestaurant);
+router.post("/add-restaurant", protect, adminOnly,  addRestaurant);
+router.put("/update-restaurant/:id", protect, adminOnly,  updateRestaurant);
+router.delete("/delete-restaurant/:id", protect, adminOnly,  deleteRestaurant);
 
 // RESERVATION ROUTES
 router.get("/get-reservations", getReservations);
-router.post("/add-reservation", addReservation);
-router.put("/update-reservation/:id", updateReservation);
-router.delete("/delete-reservation/:id", deleteReservation);
+router.post("/add-reservation", protect, addReservation);
+router.put("/update-reservation/:id", protect, updateReservation);
+router.delete("/delete-reservation/:id", protect, deleteReservation);
 
 module.exports = router;
